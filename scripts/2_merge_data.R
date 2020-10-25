b23c4852c7ee3c9d993929647eebe70057543f58
plthompson <patrickthompson9@gmail.com>
2020-10-25 12:12:52 -0700

    removing time step 0 which is a repeat of time step 1 and 5 which is a repeat of time step 4. These were improperly labelled when they were produced from video files.

--- scripts/2_merge_data.R
+++ scripts/2_merge_data.R
@@ -1,9 +1,7 @@
-#file merges data from videos on different dates
-load("./raw_data/t0/5_merged_data/Morph_mvt.RData")
-head(morph_mvt)
-morph_mvt$time <- 0
-all_morph_mvt<-morph_mvt
+#script by Patrick Thompson
+#patrick.thompson@zoology.ubc.ca
 
+#file merges data from videos on different dates
 load("./raw_data/t1/5_merged_data/Morph_mvt.RData")
 head(morph_mvt)
 morph_mvt$time <- 1
@@ -24,10 +22,5 @@ head(morph_mvt)
 morph_mvt$time <- 4
 all_morph_mvt<-rbind(all_morph_mvt,morph_mvt)
 
-load("./raw_data/t5/5_merged_data/Morph_mvt.RData")
-head(morph_mvt)
-morph_mvt$time <- 5
-all_morph_mvt<-rbind(all_morph_mvt,morph_mvt)
-
-save(all_morph_mvt,file = "./raw_data/all_morph.RData")
+save(all_morph_mvt,file = "./data/all_morph.RData")
 
