b041c1766e691a28fbeba650234ae73fd18fbdcf
plthompson <patrickthompson9@gmail.com>
2020-10-25 12:15:26 -0700

    removing exploratory scripts to just include the relevant steps.

--- scripts/4_predict_missing_data.R
+++ scripts/4_predict_missing_data.R
@@ -1,3 +1,6 @@
+#script by Patrick Thompson
+#patrick.thompson@zoology.ubc.ca
+
 #script predicts individuals in Euglena populations that were too dense for BEMOVI to predict
 #just counts particles in one time step
 library(dbscan)
@@ -10,126 +13,63 @@ all_morph_mvt$code <- tolower(all_morph_mvt$code)
 
 head(all_morph_mvt)
 morph_select <- all_morph_mvt %>% 
-  filter(time>0, time<5) %>% 
   group_by(file, code, time) %>% 
   summarise(N = n())
 
-load("./raw_data/t1/2_particle_data/particle.RData")
-head(morphology.data)
-dim(morphology.data)
+load("./data/particle_t1.RData")
 morphology.data$time <- 1
-
-unique(morphology.data$Slice)
-
 morphology.data_100 <- morphology.data[morphology.data$Slice == 300,]
-
 morphology.data_100 <- left_join(morphology.data_100, morph_select)
 
 eug_1 <- morphology.data_100 %>% 
   filter(code == "eug" | is.na(code))
-dim(eug_1)
 
-eug_1 %>% 
-  group_by(file, time, N,code) %>% 
-  summarise(N_predict = n()) %>% 
-  ggplot(aes(x = N, y = N_predict))+
-  geom_point()+
-  scale_x_log10()+
-  scale_y_log10()
-
-load("./raw_data/t2/2_particle_data/particle.RData")
-head(morphology.data)
-dim(morphology.data)
+load("./data/particle_t2.RData")
 morphology.data$time <- 2
-
-unique(morphology.data$Slice)
-
 morphology.data_100 <- morphology.data[morphology.data$Slice == 300,]
-
 morphology.data_100 <- left_join(morphology.data_100, morph_select)
 
 eug_2 <- morphology.data_100 %>% 
   filter(code == "eug" | is.na(code))
-dim(eug_2)
 
-eug_2 %>% 
-  group_by(file, time, N,code) %>% 
-  summarise(N_predict = n()) %>% 
-  ggplot(aes(x = N, y = N_predict))+
-  geom_point()+
-  scale_x_log10()+
-  scale_y_log10()
-
-load("./raw_data/t3/2_particle_data/particle.RData")
-head(morphology.data)
-dim(morphology.data)
+load("./data/particle_t3.RData")
 morphology.data$time <- 3
-
-unique(morphology.data$Slice)
-
 morphology.data_100 <- morphology.data[morphology.data$Slice == 300,]
-
 morphology.data_100 <- left_join(morphology.data_100, morph_select)
 
 eug_3 <- morphology.data_100 %>% 
   filter(code == "eug" | is.na(code))
-dim(eug_3)
 
-eug_3 %>% 
-  group_by(file, time, N,code) %>% 
-  summarise(N_predict = n()) %>% 
-  ggplot(aes(x = N, y = N_predict))+
-  geom_point()+
-  scale_x_log10()+
-  scale_y_log10()
-
-load("./raw_data/t4/2_particle_data/particle.RData")
-head(morphology.data)
-dim(morphology.data)
+load("./data/particle_t4.RData")
 morphology.data$time <- 4
-
-unique(morphology.data$Slice)
-
 morphology.data_100 <- morphology.data[morphology.data$Slice == 300,]
-
 morphology.data_100 <- left_join(morphology.data_100, morph_select)
 
 eug_4 <- morphology.data_100 %>% 
   filter(code == "eug" | is.na(code))
-dim(eug_4)
-
-eug_4 %>% 
-  group_by(file, time, N,code) %>% 
-  summarise(N_predict = n()) %>% 
-  ggplot(aes(x = N, y = N_predict))+
-  geom_point()+
-  scale_x_log10()+
-  scale_y_log10()
 
 #join_all_together####
 eug_all <- bind_rows(eug_1, eug_2, eug_3, eug_4)
-dim(eug_all)
 
 eug_pops <- eug_all %>% 
   group_by(file, time, N,code) %>% 
   summarise(N_predict = n()) %>% 
   ungroup()
 
-ggplot(eug_pops, aes(x = N, y = N_predict, color = factor(time)))+
-  geom_point()+
-  scale_x_log10()+
-  scale_y_log10()
-
-load("./pop_data.RData")
-head(pop_data)
+load("./data/pop_data.RData")
 
 eug_pops <- left_join(dplyr::select(eug_pops,-N), filter(pop_data, predicted_species == "eug"))
 head(pop_data)
 
-ggplot(eug_pops, aes(x = N, y = N_predict, color = factor(time)))+
+ggplot(eug_pops, aes(x = N, y = N_predict))+
   geom_point()+
   scale_x_log10()+
-  scale_y_log10()
+  scale_y_log10(limits = c(10, 10000))+  
+  theme_classic()+
+  geom_smooth(method = "lm")+
+  xlab("Individuals in population")+
+  ylab("Particles in single frame")
+ggsave("./figures/Euglena_estimate.pdf", height = 4, width = 4)
 
 missing_data <- filter(eug_pops, is.na(N))
 
@@ -137,12 +77,6 @@ eug_lm <- lm(log(N) ~ log(N_predict) * time, data = eug_pops)
 summary(eug_lm)
 
 exp(predict(eug_lm, newdata = missing_data))
-
-library(lme4)
-eug_lme <- lmer(formula = log(N) ~ log(N_predict) * time + (1 | file), data = eug_pops)
-summary(eug_lme)
-
-plot(exp(predict(eug_lme, newdata = missing_data, allow.new.levels = TRUE))~exp(predict(eug_lm, newdata = missing_data)))
 
 missing_data$predicted_missing <- exp(predict(eug_lm, newdata = missing_data))
 missing_data$predicted_missing[missing_data$predicted_missing<1] <- 0
@@ -157,6 +91,6 @@ missing_codes <- pop_data %>%
 
 missing_data <- left_join(ungroup(select(missing_data,file, time, predicted_missing)), select(missing_codes,-time, -N))
 
-pop_data2 <- bind_rows(pop_data,missing_data)
+light_dark_pop_data <- bind_rows(pop_data,missing_data)
 
-save(pop_data2, file = "./pop_data2.RData")
+save(light_dark_pop_data, file = "./data/light_dark_pop_data.RData")
